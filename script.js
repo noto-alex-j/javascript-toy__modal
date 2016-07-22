1c8220380cd3d6049f5ac3fa40d216e6753bf429
Alexander Noto <noto.alex.j@gmail.com>
2016-07-21 19:15:43 -0500

    modal working

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-// Replace me with your JavaScript!
+window.addEventListener("load", function(){
+
+var button = document.getElementById("button");
+
+var close = document.getElementById("close");
+
+button.addEventListener("click", function(){
+	var modal = document.getElementById("modal");
+	var transparent = document.getElementById("transparent");
+	modal.style.display = "inline-block";
+	transparent.style.display = "block";
+});
+
+close.addEventListener("click", function(){
+	var modal = document.getElementById("modal");
+	var transparent = document.getElementById("transparent");
+	modal.style.display = "none";
+	transparent.style.display = "none";
+});
+
+// close.addEventListener("click", function(){
+// 	modal.style.display = "none";
+// 	transparent.style.display = "none";
+// });
+
+
+});
